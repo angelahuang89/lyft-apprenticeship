e0d3efecb563a3c3e5b4a2972a988184d1b83316
Angela Huang <angelahuang89@gmail.com>
2018-02-02 17:43:24 -0800

    Add options response

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.options('/', (request, response) => response.json('GET,POST,PUT,PATCH,DELETE'));
+
 app.post('/test', (request, response) => {
   let output = {};
   let { x } = request.body;
   let { y } = request.body;
   let sum = x + y;
   output.sum = sum;
-  console.log(output)
   response.send(output);
 });
 
